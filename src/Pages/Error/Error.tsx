b50a7f3e1fa4e3c6f857c990ec3a525282d16693
Michael Dutheil <mike.dutheil@gmail.com>
2023-08-08 20:47:13 +0200

    Fix: 404

--- src/Pages/Error/Error.tsx
+++ src/Pages/Error/Error.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './error.css';
 
 import errorImage from '../../assets/Icon/404.svg';
@@ -6,7 +7,9 @@ function Error() {
   return (
     <div className="flex flex-col items-center justify-center">
       <p className="">404 Page Non Trouvée</p>
-
+      <Link to="/" className="text-white hover:underline">
+        Cliquez ici pour retrouver votre route
+      </Link>
       <img className="invert" src={errorImage} alt="" />
     </div>
   );
